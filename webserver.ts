638cc5a72e463e885e2019c14e1bf0e21c85c3f6
Temm <samumert@gmail.com>
2024-04-08 18:20:30 +0200

    make properly runnable

--- webserver.ts
+++ webserver.ts
@@ -3,17 +3,21 @@ import { type FullLectureData, getAllSemesterData } from "./scraper.ts"; // Impo
 let lastScrape = 0;
 let cachedData: FullLectureData[] = [];
 
-Deno.serve({
-  port: 7299,
-}, async (req) => {
-  // Have over 60 minutes passed since the last scrape?
-  const isStale = Date.now() - lastScrape > 1000 * 60 * 60;
-  if (isStale) {
-    console.log("Scraping new data");
-    cachedData = await getAllSemesterData();
-    lastScrape = Date.now();
-  }
+export function startWebserver() {
+  Deno.serve({
+    port: 7299,
+  }, async (req) => {
+    // Have over 60 minutes passed since the last scrape?
+    const isStale = Date.now() - lastScrape > 1000 * 60 * 60;
+    if (isStale) {
+      console.log("Scraping new data");
+      cachedData = await getAllSemesterData();
+      lastScrape = Date.now();
+    }
 
-  console.log("Serving request");
-  return new Response(JSON.stringify(cachedData));
-});
+    console.log("Serving request");
+    return new Response(JSON.stringify(cachedData));
+  });
+  
+  console.log("Webserver started on port 7299");
+}
